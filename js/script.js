e74052d6aaaaf2f8b14cc5329d0c8c6460825c67
KevinSonda <kevinsonda@gmail.com>
2024-07-03 03:35:48 +0200

    [UPD]Fine eserecizio

--- js/script.js
+++ js/script.js
@@ -6,22 +6,33 @@ btn.addEventListener("click", function () {
 
 //quando il pulsante viene clikkato recupera i dati quindi i km e l'eta
     const km = document.getElementById("km").value;
-    const age = document.getElementById("age")
+    const age = document.getElementById("age").value;
 
     //calcolo il prezzo base
-    let price = km * 0.21;
-    let finale_price
+    let final_price = km * 0.21;
+    let message = `il prezzo del tuo biglietto e pari a ${final_price.toFixed(2)}`;
     //verifico l'eta del passegero che a seconda del valore e applico lo sconto
-    if(age == 0){//maggiorenne
-        finale_price = price;
+    
+    if (age == 1){ //minorenne
+        final_price  *= 0.8;
+        message = `il prezzo del tuo biglietto e pari a ${final_price.toFixed(2)}`;
     }
-    else if(age == 1){ //minorenne
-        finale_price = price * 0.8;
+    else if (age == 2){//over 65
+        final_price  *= 0.6;
     }
-    else if(age == 2){//over 65
-        finale_price = price * 0.6;
-    }
-    else {
-        alert("non hai selezionato l\'eta. verra applicato il costo base");
-    }
+    
+
+    console.log(document.getElementById('message').innerHTML);
+    document.getElementById('message').innerHTML = message;
+
+
+});
+
+btn_reset.addEventListener('click', function (){
+    
+    document.getElementById('km').value = '';
+    document.getElementById('age').value = '';
+
+    document.getElementById('message').innerHTML = '';
 })
+
